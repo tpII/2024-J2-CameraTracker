3ba9fd7f292b4e6835be6ac55cc4b8e063daddef
lapor <francolaporta10@gmail.com>
2024-12-09 18:59:44 -0300

    Actualización del Código

---
+++ CameraWebServer/web_server.h
@@ -0,0 +1,10 @@
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h" //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include "esp_http_server.h"
+
+#define PART_BOUNDARY "123456789000000000000987654321"
